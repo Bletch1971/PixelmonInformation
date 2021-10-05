d727d654df1ce1aad9fc43b49dcb97cd0bc9c6d5
Brett Hewitson <bletch1971@hotmail.com>
2021-10-05 22:15:41 +1000

    new build files

---
+++ src/6.3.4/build - Release.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set WORKDIR=%CD%
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+
+cd /d "%DIRNAME%"
+
+rmdir build\classes /S /Q
+rmdir build\sources /S /Q
+rmdir build\resources /S /Q
+
+call gradlew.bat build
+
+xcopy "build\libs\*.jar" "..\..\6.3\*.jar" /Y /Q
+xcopy "updateforge.json" "..\..\6.3\updateforge.json" /Y /Q
+
+cd /d "%WORKDIR%"
+
+pause
